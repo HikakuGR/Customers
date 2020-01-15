3c5ad8945bff54f474f4ac61a208ef0ef8c0d771
hikaku <petkostas@hotmaill.com>
2020-01-15 20:28:30 +0200

    Add project files.

---
+++ BlazorApp/Controllers/CustomersController.cs
@@ -0,0 +1,74 @@
+﻿using BlazorApp.Models;
+using BlazorApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BlazorApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerService _CustomerService;
+
+        public CustomersController(CustomerService CustomerService)
+        {
+            _CustomerService = CustomerService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Customer>> Get() =>
+            _CustomerService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetCustomer")]
+        public ActionResult<Customer> Get(string id)
+        {
+            var Customer = _CustomerService.Get(id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            return Customer;
+        }
+
+        [HttpPost]
+        public ActionResult<Customer> Create(Customer Customer)
+        {
+            _CustomerService.Create(Customer);
+
+            return CreatedAtRoute("GetCustomer", new { id = Customer.Id.ToString() }, Customer);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Customer CustomerIn)
+        {
+            var Customer = _CustomerService.Get(id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            _CustomerService.Update(id, CustomerIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var Customer = _CustomerService.Get(id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+
+            _CustomerService.Remove(Customer.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ BlazorApp/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
