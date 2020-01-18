cd7cc9d77636e9ca2d5b9ab653732148ead521a4
hikaku <petkostas@hotmaill.com>
2020-01-18 15:24:11 +0200

    Telerik Grid

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -45,6 +45,8 @@ namespace BlazorApp
             //services.AddSingleton<WeatherForecastService>();
 
             services.AddControllers();
+
+            services.AddTelerikBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
