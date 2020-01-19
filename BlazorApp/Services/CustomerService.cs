c0cc23a33e122b967350188199fd253660cb0e77
hikaku <petkostas@hotmaill.com>
2020-01-19 14:20:01 +0200

    Telerik Server-Side Paging done CRUD broke added Telerik Dll files

--- BlazorApp/Services/CustomerService.cs
+++ BlazorApp/Services/CustomerService.cs
@@ -23,6 +23,12 @@ namespace BlazorApp.Services
         public Customer Get(string id) =>
             _Customers.Find<Customer>(Customer => Customer.Id == id).FirstOrDefault();
 
+        public List<Customer> GetPaged(int page, int pageSize, out int total)
+        {
+            var customers = _Customers.Find(Customer => true).Skip(page * pageSize).Limit(pageSize).ToList();
+            total = (int)_Customers.CountDocuments(Customer => true);
+            return customers;
+        }
         public Customer Create(Customer Customer)
         {
             _Customers.InsertOne(Customer);
